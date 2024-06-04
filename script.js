b00ed9933e225754e16033f3e71bab03f2b50a34
shajiyakhan1309 <143182558+shajiyakhan1309@users.noreply.github.com>
2024-06-04 14:27:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let box = document.getElementById("main-box");
+let info = document.getElementById("info");
+let temp = document.querySelector(".temp");
+let fahn = document.querySelector(".Fahren");
+let wind = document.querySelector(".wind");
+let time = document.querySelector(".time");
+
+// Getting Kolkata Timing Api
+const URL = "http://worldtimeapi.org/api/timezone/Asia/Kolkata";
+const getTime = async()=>{
+    console.log("getting data.....");
+    let response = await fetch(URL);
+    console.log(response);
+    let data = await response.json();
+    time.innerText = "Date & Time : "+ data.datetime;
+    
+}
+
+// Getting Kolkata weather Api
+const URL2 = "https://wttr.in/Kolkata?format=j1";
+const getTemp = async()=>{
+    let response2= await fetch(URL2);
+    console.log(response2);
+    let data2 = await response2.json();
+    temp.innerText = "Temperature in Celsius : " + data2.current_condition[0].temp_C + "\u00B0C";
+    fahn.innerText = "Temperature in Fahrenheit  : " + data2.current_condition[0].temp_F + "\u00B0F";
+    console.log(temp.innerText);
+
+    // For Sunny day
+    if(temp.innerText >= "Temperature in Celsius : 26\u00B0C"){
+        box.style.backgroundImage="url('Sunnyday.jpg')";
+        info.style.color = "black";
+        wind.innerText = "Sunny";
+    }
+
+    // For Chilly day
+    else if(temp.innerText >= "Temperature in Celsius : 0\u00B0C" && temp.innerText <= "Temperature in Celsius : 26\u00B0C"){
+        box.style.backgroundImage="url('winterday.jpg')";
+        info.style.color = "black";
+        wind.innerText = "Chilly";
+    }
+    
+    // For Rainy day
+    else{
+        box.style.backgroundImage="url('Rainyday.jpg')";
+        info.style.color = "white";
+        wind.innerText = "Rainy";
+    }
+}
+
+
+let currSeason = "Rainyday.jpg";
+console.log(currSeason);
+getTime();
+getTemp();
+
+console.log(currSeason);
+
+
+
+
